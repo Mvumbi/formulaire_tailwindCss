2b25a7b647f5405683a42e6922d74c8d4349c1c5
Mvumbi Mabiala <simpsonpro08@gmail.com>
2024-11-14 14:26:44 +0100

    form

---
+++ index.js
@@ -0,0 +1,9 @@
+const nom = document.querySelector('name');
+const prenom = document.querySelector('prenom');
+const num = document.querySelector('num');
+const Btn = document.querySelector('#submit');
+
+Btn.addEventListener('click', ()=>{
+   body.style.background = 'red';
+    
+})
